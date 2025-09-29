8d2ea7d5de6a034940e7c66d84f514d2f22f93e0
Asad <asadullahkhan0098@gmail.com>
2025-09-29 13:15:19 +0530

    Make computer ship placement more random

--- src/players.js
+++ src/players.js
@@ -7,7 +7,13 @@ function createPlayer(type) {
     const shipLengths = [5,4,3,3,2];
     for (let i = 0; i < 5; i++) {
       const x = Math.floor(Math.random() * 10);
-      const y = i;
+      const emptyRows = gameboard.board.filter(row => row.every(item => item === null));
+      const indexesOfEmptyRows = []; 
+      emptyRows.forEach(emptyRow => {
+        indexesOfEmptyRows.push(gameboard.board.indexOf(emptyRow));
+      });
+      const randomIndexOfEmptyRow = Math.floor(Math.random() * indexesOfEmptyRows.length);
+      const y = indexesOfEmptyRows[randomIndexOfEmptyRow];
       gameboard.placeShip(x,y,shipLengths[i]);
     }
     let isCom = true;
