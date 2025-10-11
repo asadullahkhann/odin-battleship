66fb8a4bd678469cfbf7ab246e8d68a35e5d36bd
Asad <asadullahkhan0098@gmail.com>
2025-10-11 12:18:37 +0530

    Set softer white for bg color
    make header & footer use the same font
    show radio buttons below labels
    change github mark
    refactor some code to improve readability

--- src/event-handlers.js
+++ src/event-handlers.js
@@ -7,7 +7,7 @@ import {
 
 let players = {};
 
-const getUiGameboards = () => document.querySelectorAll('main > div');
+const getUiBoards = () => document.querySelectorAll('main > div');
 const getCells = () => Array.from(document.querySelectorAll('.cell'));
 const getRandomCoordinates = () => {
   const emptyCells = getCells().slice(0,100).filter(cell => !cell.firstChild)
@@ -18,87 +18,70 @@ const getRandomCoordinates = () => {
 }
 
 const handleShipPlacment = (e) => {
-  const uiGameboards = getUiGameboards();
+  const [uiBoard1, uiBoard2] = getUiBoards();
   const cells = getCells();
-  const coordinates = e.target.getAttribute('data-coordinates');
-  const x = +coordinates[0];
-  const y = +coordinates[1];
-  if (e.target.parentNode === uiGameboards[0] &&
-    !players.player1.gameboard.allShipsPlaced()
-  ) {
-    players.player1.gameboard.placeShip(x,y);
-    placeShipOnUi(players.player1.gameboard.board, uiGameboards[0]);
-    e.target.removeEventListener('click', handleShipPlacment);
-  } else if (e.target.parentNode === uiGameboards[1] && 
-    !players.player2.gameboard.allShipsPlaced()
-    ) {
-      players.player2.gameboard.placeShip(x,y);
-      placeShipOnUi(players.player2.gameboard.board, uiGameboards[1]);
-      e.target.removeEventListener('click', handleShipPlacment);
+  const [x, y] = e.target.getAttribute('data-coordinates');
+  const targetParent = e.target.parentNode;
+  const {player1, player2} = players;
+  if (targetParent === uiBoard1 && !player1.gameboard.allShipsPlaced()) {
+    player1.gameboard.placeShip(+x, +y);
+    placeShipOnUi(player1.gameboard.board, uiBoard1);
+  } else if (targetParent === uiBoard2 && !player2.gameboard.allShipsPlaced()) {
+      player2.gameboard.placeShip(+x, +y);
+      placeShipOnUi(player2.gameboard.board, uiBoard2);
     }
-  if (players.player1.gameboard.allShipsPlaced()) {
-    cells.slice(0, 100).filter(cell => cell.classList.contains('border-y')).forEach(cell => {
+  if (player1.gameboard.allShipsPlaced()) {
+    cells.slice(0, 100).forEach(cell => {
       setTimeout(() => {
         cell.classList = 'cell';
       }, 1000);
     })
   }
-  if (players.player2.gameboard.allShipsPlaced()) {
-    cells.slice(100).filter(cell => cell.classList.contains('border-y')).forEach(cell => {
+  if (player2.gameboard.allShipsPlaced()) {
+    cells.slice(100).forEach(cell => {
       setTimeout(() => {
         cell.classList = 'cell';
       }, 1000);
     })
   }
-  if (players.player1.gameboard.allShipsPlaced() &&
-    players.player2.gameboard.allShipsPlaced()
-  ) {
+  if (player1.gameboard.allShipsPlaced() && player2.gameboard.allShipsPlaced()) {
     cells.forEach(cell => {
       cell.removeEventListener('click', handleShipPlacment);
       setTimeout(() => {
         cell.addEventListener('click', handleShipAttack);
+        if (player2.isCom && cell.parentNode === uiBoard1) {
+          cell.removeEventListener('click', handleShipAttack);
+        }
       }, 1000);
     });
-    if (players.player2.isCom) {
-      setTimeout(() => {
-        cells.slice(0,100).forEach(cell => {
-        cell.removeEventListener('click', handleShipAttack);
-      });
-      }, 1000);
-    }
   }
 }
 
 const handleShipAttack = (e) => {
-  const uiGameboards = getUiGameboards();
-  const coordinates = e.target.getAttribute('data-coordinates');
-  const x = +coordinates[0];
-  const y = +coordinates[1];
-  switch (e.target.parentNode) {
-    case uiGameboards[0]:
-      players.player1.gameboard.receiveAttack(x,y);
-      placeAttackOnUi(x, y, players.player1.gameboard.board, uiGameboards[0]);
+  const [uiBoard1, uiBoard2] = getUiBoards();
+  const [x, y] = e.target.getAttribute('data-coordinates');
+  const targetParent = e.target.parentNode;
+  const {player1, player2} = players;
+  const opponent = player2.isCom ? 'Computer' : 'Player 2';
+  if (targetParent === uiBoard1) {
+    player1.gameboard.receiveAttack(+x, +y);
+    placeAttackOnUi(+x, +y, player1.gameboard.board, uiBoard1);
+    e.target.removeEventListener('click', handleShipAttack);
+  } else if (targetParent === uiBoard2) {
+      player2.gameboard.receiveAttack(+x, +y);
+      placeAttackOnUi(+x, +y, player2.gameboard.board, uiBoard2);
       e.target.removeEventListener('click', handleShipAttack);
-      break;
-    case uiGameboards[1]: 
-      players.player2.gameboard.receiveAttack(x,y);
-      placeAttackOnUi(x, y, players.player2.gameboard.board, uiGameboards[1]);
-      if (players.player2.isCom) {
-        const coordinates = getRandomCoordinates();
-        const x = +coordinates[0];
-        const y = +coordinates[1];
-        players.player1.gameboard.receiveAttack(x,y);
-        placeAttackOnUi(x, y, players.player1.gameboard.board, uiGameboards[0]);
-      };
-      e.target.removeEventListener('click', handleShipAttack);
-      break;
   };
-  const opponent = players.player2.isCom ? 'Computer' : 'Player 2';
-  if (players.player1.gameboard.allShipsSunk()) {
+  if (player2.isCom) {
+    const [x, y] = getRandomCoordinates();
+    player1.gameboard.receiveAttack(+x, +y);
+    placeAttackOnUi(+x, +y, player1.gameboard.board, uiBoard1);
+  };
+  if (player1.gameboard.allShipsSunk()) {
     showWinningMessage(`${opponent} wins — all Player 1 ships sunk`);
-  } else if (players.player2.gameboard.allShipsSunk()) {
-    showWinningMessage(`Player 1 wins — all ${opponent} ships sunk`);
-  }
+  } else if (player2.gameboard.allShipsSunk()) {
+      showWinningMessage(`Player 1 wins — all ${opponent} ships sunk`);
+  };
 };
 
 const handlePlayBtnClick = () => { 
