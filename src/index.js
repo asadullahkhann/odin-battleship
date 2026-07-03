266f78379bf28d2686ffcfca279bdde0d458b820
Asad <asadullahkhan0098@gmail.com>
2026-07-04 01:26:22 +0530

    Make footer content smaller
    - Prevent animations on page load
    - Replace dropdown pointer-up image with pseudo element

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles.css';
 import { renderGameboard } from './game-controller';
 
 window.onload = () => {
+  document.querySelector('body').classList.remove('preload');
   renderGameboard();
 };
