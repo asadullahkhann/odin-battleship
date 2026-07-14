e593944028520c1e73d7181357e93c44a9966fca
Asad <asadullahkhan0098@gmail.com>
2026-07-14 17:39:20 +0530

    Use transition for dropdown collapse animation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import './styles.css';
 import { renderGameboard } from './game-controller';
 
 window.onload = () => {
-  document.querySelector('body').classList.remove('preload');
   renderGameboard();
 };
